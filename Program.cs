c03aa36548fa9b08d71eab320ec84e7a4e31960d
johnbean36 <jmbass36@gmail.com>
2024-07-30 17:40:54 -0500

    Working on the lab

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace RoverControlCenter
 {
@@ -6,11 +8,25 @@ namespace RoverControlCenter
   {
     static void Main(string[] args)
     {
-      MoonRover lunokhod = new MoonRover("Lunokhod 1", 1970);
-      MoonRover apollo = new MoonRover("Apollo 15", 1971);
-      MarsRover sojourner = new MarsRover("Sojourner", 1997);
-      Satellite sputnik = new Satellite("Sputnik", 1957); 
-  		
+        MoonRover lunokhod = new MoonRover("Lunokhod 1", 1970);
+        MoonRover apollo = new MoonRover("Apollo 15", 1971);
+        MarsRover sojourner = new MarsRover("Sojourner", 1997);
+        Satellite sputnik = new Satellite("Sputnik", 1957); 
+        Rover[] rover = new Rover[]{lunokhod, apollo, sojourner};
+        DirectAll(rover);
+        Object[] probes = {lunokhod, apollo, sojourner, sputnik};
+        foreach(Object space in probes){
+            Console.WriteLine($"Tracking a {space.GetType()}");
+        }
     }
+
+    public static void DirectAll(Rover[] aRover){
+        foreach(Rover item in aRover){
+            Console.WriteLine(item.GetInfo());
+            Console.WriteLine(item.Explore());
+            Console.WriteLine(item.Collect());
+        }
+    }
+
   }
 }
